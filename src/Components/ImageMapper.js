bc3bd7250faa74125538092676754f80ec931973
DEVGEEKTM <ismailostmar@gmail.com>
2022-06-07 16:31:20 +0100

    Adding Coordinates and changing colors

--- src/Components/ImageMapper.js
+++ src/Components/ImageMapper.js
@@ -6,33 +6,55 @@ import ImageMapper from "react-img-mapper";
 
 function Imagemap() {
     const [areas, setAreas] = useState([
+        /*X , Y , Rayon*/
         {
             id: "test-0",
             shape: "circle",
-            coords: [200, 78, 5],
+            coords: [690, 133, 5],
             fillColor: "yellow",
-            preFillColor: "#fa0000"
+            preFillColor: "#00fa19"
         },
         {
             id: "test-1",
             shape: "circle",
-            coords: [300, 78, 5],
+            coords: [690, 158, 5],
             fillColor: "yellow",
-            preFillColor: "#fa0000"
+            preFillColor: "#00fa19"
         },
         {
             id: "test-2",
             shape: "circle",
-            coords: [400, 78, 5],
+            coords: [690, 182, 5],
             fillColor: "yellow",
-            preFillColor: "#fa0000"
+            preFillColor: "#00fa19"
         },
         {
             id: "test-3",
             shape: "circle",
-            coords: [500, 78, 5],
+            coords: [640, 133, 5],
             fillColor: "yellow",
-            preFillColor: "#fa0000"
+            preFillColor: "#00fa19"
+        },
+        {
+            id: "test-4",
+            shape: "circle",
+            coords: [640, 158, 5],
+            fillColor: "yellow",
+            preFillColor: "#00fa19"
+        },
+        {
+            id: "test-5",
+            shape: "circle",
+            coords: [640, 181, 5],
+            fillColor: "yellow",
+            preFillColor: "#00fa19"
+        },
+        {
+            id: "test-6",
+            shape: "circle",
+            coords: [608, 133, 5],
+            fillColor: "yellow",
+            preFillColor: "#00fa19"
         }
     ]);
 
@@ -42,45 +64,45 @@ function Imagemap() {
     };
 
     return (
-        <MapInteraction>
-            {({ translation, scale }) => {
-                return (
-                    <div
-                        style={{
-                            transform: `translate(${translation.x}px, ${translation.y}px) scale(${scale})`,
-                            transformOrigin: "0px 0px"
-                        }}
-                    >
-                        <ImageMapper
-                            src="images/plantest.png"
-                            width={1000}
-                            height={400}
-                            imgWidth={800}
-                            onClick={(clicked) => {
-                                console.log("clicked ", clicked, areas);
-                                setAreas((prevState) => {
-                                    return prevState.map((item) => {
-                                        if (item.id === clicked.id) {
-                                            return {
-                                                ...item,
-                                                preFillColor: "green"
-                                            };
-                                        }
+        // <MapInteraction>
+        //     {({ translation, scale }) => {
+        //         return (
+        //             <div
+        //                 style={{
+        //                     transform: `translate(${translation.x}px, ${translation.y}px) scale(${scale})`,
+        //                     transformOrigin: "0px 0px"
+        //                 }}
+        //             >
+        <ImageMapper
+            src="images/plantest.png"
+            width={950}
+            height={320}
+            imgWidth={800}
+            onClick={(clicked) => {
+                console.log("clicked ", clicked, areas);
+                setAreas((prevState) => {
+                    return prevState.map((item) => {
+                        if (item.id === clicked.id) {
+                            return {
+                                ...item,
+                                preFillColor: "red"
+                            };
+                        }
 
-                                        return item;
-                                    });
-                                });
-                            }}
-                            onMouseEnter={(Hovered) => {
-                                console.log("Hovered ", Hovered);
-                                Hovered.fillColor = "yellow";
-                            }}
-                            map={map}
-                        />
-                    </div>
-                );
+                        return item;
+                    });
+                });
             }}
-        </MapInteraction>
+            onMouseEnter={(Hovered) => {
+                console.log("Hovered ", Hovered);
+                Hovered.fillColor = "yellow";
+            }}
+            map={map}
+        />
+        //             </div>
+        //         );
+        //     }}
+        // </MapInteraction>
     );
 };
 Imagemap.propTypes = {
